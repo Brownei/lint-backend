c26c9ce91fc4c6c589ef325cc72cd7c7b3e197a7
Brownson <esitibrownson@gmail.com>
2024-04-29 15:16:00 +0100

    Few days till launch

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as session from 'express-session';
+import * as cookieParser from 'cookie-parser';
+import {NestExpressApplication} from '@nestjs/platform-express';
 
 async function server() {
   const docsRoute = 'docs';
   const port = process.env.APP_PORT || 3000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const config = new DocumentBuilder()
     .setTitle('LinT Api')
@@ -16,11 +19,22 @@ async function server() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(docsRoute, app, document);
 
+  app.use(
+    session({
+      secret: process.env.JWT_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
 
   app.enableCors({
     origin: ['http://localhost:5173'],
     credentials: true,
   });
+  app.use(cookieParser());
 
   app.useGlobalPipes(new ValidationPipe());
 
