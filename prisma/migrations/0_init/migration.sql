d9bf403ab1ebda94fcab005c14a912f48f51710d
brownson <esitibrownson@gmail.com>
2024-07-02 16:14:23 +0100

    More hero changes and to the database too

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -1,9 +1,6 @@
 -- CreateEnum
 CREATE TYPE "Status" AS ENUM ('accepted', 'pending', 'rejected');
 
--- CreateEnum
-CREATE TYPE "Brown" AS ENUM ('deep', 'heavy');
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
